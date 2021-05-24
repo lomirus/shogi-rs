76eefb80d1897412f3778888c4a807a739dd799e
Rhys <lomirus@qq.com>
2021-05-24 08:30:10 +0800

    feat: hide cursor

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,26 @@
 mod chessboard;
 mod piece;
-use crossterm::{terminal, Result};
+use crossterm::{terminal, cursor, Result, ExecutableCommand};
+use std::io::{stdout};
 
 fn main() -> Result<()> {
-    terminal::enable_raw_mode()?;
+    before_main()?;
 
     let chessboard = chessboard::new();
     chessboard.print()?;
     chessboard.listen()?;
 
+    after_main()
+}
+
+fn before_main() -> Result<()> {
+    let mut stdout = stdout();
+    stdout.execute(cursor::Hide)?;
+    terminal::enable_raw_mode()?;
+    Ok(())
+}
+
+fn after_main() -> Result<()> {
     terminal::disable_raw_mode()?;
     Ok(())
 }
